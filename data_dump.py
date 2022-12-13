8dda2f069eff71f14cca54ab60e1750c64cd544d
yashwantram97 <yash97r@gmail.com>
2022-12-13 16:26:47 +0530

    config related changes

--- data_dump.py
+++ data_dump.py
@@ -1,26 +1,21 @@
 import pymongo
 import pandas as pd
 import json
-from dotenv import load_dotenv
-print("Loading enviroment varibale from .env file")
-load_dotenv()
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb+srv://admin:getSOMETHINGnew_009@cluster0.2sdtbef.mongodb.net/?retryWrites=true&w=majority")
+from sensor.config import mongo_client
 
-DATA_FILE_PATH="https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv"
+DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
 COLLECTION_NAME="sensor"
 
-if __name__ == "__main__":
+if __name__=="__main__":
     df = pd.read_csv(DATA_FILE_PATH)
-    print(f"ROWS AND COLUMNS: {df.shape}")
+    print(f"Rows and columns: {df.shape}")
 
-    #convert dataframe to json so that we can dump these records in mongo db
+    #Convert dataframe to json so that we can dump these record in mongo db
     df.reset_index(drop=True,inplace=True)
 
-    json_records = list(json.loads(df.T.to_json()).values())
-    print(json_records[0]) 
-
-    #insert converted json record json to mongo db
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_records)
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+    #insert converted json record to mongo db
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
--- sensor/config.py
+++ sensor/config.py
@@ -2,6 +2,7 @@ import pymongo
 import pandas as pd
 import json
 from dataclasses import dataclass
+# Provide the mongodb localhost url to connect python to mongodb.
 import os
 
 @dataclass
@@ -10,9 +11,7 @@ class EnvironmentVariable:
     aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
     aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-# Provide the mongodb localhost url to connect python to mongodb.
-env_var = EnvironmentVariable()
-mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+
 env_var = EnvironmentVariable()
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
 TARGET_COLUMN = "class"
